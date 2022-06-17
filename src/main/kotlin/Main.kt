02937fe1f037ae620556d6d2bad6c24a18af3f32
Dmitriy <dyakuta@mail.ru>
2022-06-17 14:34:22 +0300

    Arrays

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,18 @@
+import kotlin.random.Random
+
 fun main(args: Array<String>) {
-    println("Hello World!")
-    println("New words")
-    println("New work")
-   }
+
+    val list = listOf(1, 2, 3, 4, 5)
+
+    val newList = list.map { it + it }
+
+    newList.forEach{println(it)}
+
+}
+
+fun printArray(item: List<Any>) {
+    for (i in 1..item.size) {
+        println("Element #$i - ${item[i-1]}")
+    }
+
+}
